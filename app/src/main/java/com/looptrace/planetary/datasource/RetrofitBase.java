fbfc83fd68013c6f9dfaa4948d82fc0b51648467
Onwe Onweokoh-Nwaoji Chukwumaobim <kr8tiveonwe@gmail.com>
2020-10-09 16:29:23 +0100

    setup folder structure

---
+++ app/src/main/java/com/looptrace/planetary/datasource/RetrofitBase.java
@@ -0,0 +1,26 @@
+package com.looptrace.planetary.datasource;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitBase {
+
+    private static RetrofitBase instance = null;
+    public static final String GET_BASE_URL = "https://gadsapi.herokuapp.com/api/";
+    public static final String POST_BASE_URL = "https://docs.google.com/forms/d/e/";
+
+    public static RetrofitBase getInstance() {
+        if (instance == null) {
+            instance = new RetrofitBase();
+        }
+
+        return instance;
+    }
+
+    public Retrofit buildRetrofit(String BaseUrl) {
+        return new Retrofit.Builder()
+                .baseUrl(BaseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+}
